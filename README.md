fd717d25d6dcdb71cd56c4b0176dd99b1cde5962
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2016-10-01 00:48:09 +0900

    1st impl

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # l2h
+
 AWS Lambda HTTP Proxy integration event bridge to Go net/http.
+
+## Example
+
+- API Gateway with Lambda Proxy Integration through a Proxy Resource
+- [Apex](http://apex.run/)
+
+```go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/apex/go-apex"
+	"github.com/fujiwara/l2h"
+)
+
+var mux = http.NewServeMux()
+
+func init() {
+	mux.HandleFunc("/", handleRoot)
+	mux.HandleFunc("/hello", handleHello)
+}
+
+func main() {
+	if os.Getenv("APEX") == "" {
+		log.Println("starting up with local httpd")
+		log.Fatal(http.ListenAndServe(":8080", mux))
+	}
+	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
+		r, err := l2h.NewRequest(event)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		w := l2h.NewResponseWriter()
+		mux.ServeHTTP(w, r)
+		return w.Response(), nil
+	})
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "Hello %s\n", r.FormValue("name"))
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintln(w, "Hello World")
+	fmt.Fprintln(w, r.URL)
+}
+```
+
+## LICENSE
+
+The MIT License (MIT)
+
+Copyright (c) 2016 FUJIWARA Shunichiro
---
+++ l2h.go
@@ -0,0 +1,125 @@
+package l2h
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Request struct {
+	Body                  string            `json:"body"`
+	Headers               map[string]string `json:"headers"`
+	HTTPMethod            string            `json:"httpMethod"`
+	Path                  string            `json:"path"`
+	PathParameters        map[string]string `json:"pathParameters"`
+	QueryStringParameters map[string]string `json:"queryStringParameters"`
+	Resource              string            `json:"resource"`
+	StageVariables        map[string]string `json:"stageVariables"`
+	RequestContext        RequestContext    `json:"requestContext"`
+}
+
+func NewRequest(event json.RawMessage) (*http.Request, error) {
+	var r Request
+	if err := json.Unmarshal(event, &r); err != nil {
+		return nil, err
+	}
+	return r.HTTPRequest(), nil
+}
+
+type RequestBody struct {
+	*strings.Reader
+}
+
+func (b *RequestBody) Close() error {
+	return nil
+}
+
+func (r Request) HTTPRequest() *http.Request {
+	header := make(http.Header)
+	for key, value := range r.Headers {
+		header.Add(key, value)
+	}
+	host := header.Get("Host")
+	header.Del("Host")
+	formV := make(url.Values)
+	for key, value := range r.QueryStringParameters {
+		formV.Add(key, value)
+	}
+	uri := r.Path
+	if len(r.QueryStringParameters) > 0 {
+		uri = uri + "?" + formV.Encode()
+	}
+	u, _ := url.Parse(uri)
+	req := http.Request{
+		Method:        r.HTTPMethod,
+		Proto:         "HTTP/1.1",
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		Header:        header,
+		ContentLength: int64(len(r.Body)),
+		Body:          &RequestBody{strings.NewReader(r.Body)},
+		RemoteAddr:    r.RequestContext.Identity["sourceIp"],
+		Form:          formV,
+		Host:          host,
+		RequestURI:    uri,
+		URL:           u,
+	}
+	return &req
+}
+
+type RequestContext struct {
+	AccountID    string            `json:"accountId"`
+	ApiID        string            `json:"apiId"`
+	HTTPMethod   string            `json:"httpMethod"`
+	Identity     map[string]string `json:"identity"`
+	RequestID    string            `json"requestId"`
+	ResourceID   string            `json:"resourceId"`
+	ResourcePath string            `json:"resourcePath"`
+	Stage        string            `json:"stage"`
+}
+
+type Response struct {
+	StatusCode int               `json:"statusCode"`
+	Headers    map[string]string `json:"headers"`
+	Body       string            `json:"body"`
+}
+
+func NewResponseWriter() *ResponseWriter {
+	return &ResponseWriter{
+		statusCode: http.StatusOK,
+		header:     make(http.Header),
+		body:       []byte{},
+	}
+}
+
+type ResponseWriter struct {
+	header     http.Header
+	body       []byte
+	statusCode int
+}
+
+func (w *ResponseWriter) Header() http.Header {
+	return w.header
+}
+
+func (w *ResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+}
+
+func (w *ResponseWriter) Write(b []byte) (int, error) {
+	w.body = append(w.body, b...)
+	return len(b), nil
+}
+
+func (w *ResponseWriter) Response() Response {
+	h := make(map[string]string, len(w.header))
+	for key, _ := range w.header {
+		h[key] = w.header.Get(key)
+	}
+	return Response{
+		StatusCode: w.statusCode,
+		Headers:    h,
+		Body:       string(w.body),
+	}
+}
