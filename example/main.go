3e095aa755810dd03daeaabbfdda675c39b40fd1
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-06-20 02:43:54 +0900

    enables proxy protocol for example.

--- example/main.go
+++ example/main.go
@@ -16,18 +16,19 @@ func init() {
 }
 
 func main() {
+	ridge.ProxyProtocol = true
 	ridge.Run(":8080", "/", mux)
 }
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	log.Println("handleHello", r.URL)
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Hello %s\n", r.FormValue("name"))
+	fmt.Fprintf(w, "Hello %s from %s\n", r.FormValue("name"), r.RemoteAddr)
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Println("handleRoot", r.URL)
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintln(w, "Hello World")
+	fmt.Fprintf(w, "Hello World from %s", r.RemoteAddr)
 	fmt.Fprintln(w, r.URL)
 }
